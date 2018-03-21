37f67576310e064f38e0771fc0d752c5e346bc35
Charles Duporge <charles.duporge@ynov.com>
2018-03-21 12:18:53 +0100

    Checkbox call new method 'is_seen' to update 'seen' value in episode

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :reviews
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :episodes do
+  	get :is_seen
+  end
   resources :series
 end
